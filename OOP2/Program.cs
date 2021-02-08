36f7600b1f20c29b28f6f30e0eec509ad290c11b
dilara-tasci <dilara-tasci@github.com>
2021-02-08 16:24:36 +0300

    23 Ocak ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demiroğ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri(); //new'den sonraki kısım referans kısmı
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1); //referans olduğundan her iki tür de sorun çıkarmadan eklenebiliyor
+            musteriManager.Add(musteri2);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Grup1
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            ihtiyacKrediManager.Hesapla();
+
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            tasitKrediManager.Hesapla();
+
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+            konutKrediManager.Hesapla();
+
+            //Grup2
+            IKrediManager ihtiyacKrediManager2 = new IhtiyacKrediManager();
+            ihtiyacKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager2 = new TasitKrediManager();
+            tasitKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager2 = new KonutKrediManager();
+            konutKrediManager.Hesapla();
+
+            //Grup1 ve Grup2 sorunsuz şekilde çalışır. Yani interface'ler de referans numarası tutar.
+            */
+
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService fileLoggerService = new FileLoggerService();
+            List<ILoggerService> loggers = new List<ILoggerService>() { new DatabaseLoggerService(), new SmsLoggerService()};
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, loggers);
+           // basvuruManager.BasvuruYap(new EsnafKrediManager(), new SmsLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
