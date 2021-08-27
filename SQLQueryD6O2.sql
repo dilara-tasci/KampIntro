5e5ad45ee7f03b4958fda12d62729860f488ace1
dilara-tasci <dilara-tasci@github.com>
2021-08-27 13:44:33 +0300

    27 ocak ödev 2 tamamlandı

---
+++ SQLQueryD6O2.sql
@@ -0,0 +1,5 @@
+﻿select p.productName [ürün adı], sum(od.UnitPrice*od.Quantity)[Kazanılan Toplam Miktar] 
+from Orders o 
+inner join (Products p 
+			inner join [Order Details] od on p.ProductID=od.ProductID) 
+on o.OrderID=od.OrderID group by p.ProductName; 
